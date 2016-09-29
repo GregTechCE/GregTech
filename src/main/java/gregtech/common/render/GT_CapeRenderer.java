d665022f4f054fe1a56d8b6ac0513a3ff5d8ed53
Blood-Asp <bloodasphendrik@gmail.com>
2016-09-29 16:21:33 +0200

    Reenable GregTech Capes

--- src/main/java/gregtech/common/render/GT_CapeRenderer.java
+++ src/main/java/gregtech/common/render/GT_CapeRenderer.java
@@ -1,8 +1,17 @@
 package gregtech.common.render;
 
+import gregtech.api.enums.GT_Values;
+import gregtech.api.util.GT_Log;
+import gregtech.api.util.GT_Utility;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import org.lwjgl.opengl.GL11;
 
 import java.util.Collection;
 
@@ -10,16 +19,13 @@ public class GT_CapeRenderer
         extends RenderPlayer {
     private final ResourceLocation[] mCapes = {new ResourceLocation("gregtech:textures/BrainTechCape.png"), new ResourceLocation("gregtech:textures/GregTechCape.png"), new ResourceLocation("gregtech:textures/MrBrainCape.png"), new ResourceLocation("gregtech:textures/GregoriusCape.png")};
     private final Collection<String> mCapeList;
-    private static final float glf0 = 0.0F;
-    private static final float glf1 = 1.0F;
 
     public GT_CapeRenderer(Collection<String> aCapeList) {
         this.mCapeList = aCapeList;
         setRenderManager(RenderManager.instance);
     }
 
-    //always off in BMPL
-    /*public void receiveRenderSpecialsEvent(RenderPlayerEvent.Specials.Pre aEvent) {
+    public void receiveRenderSpecialsEvent(RenderPlayerEvent.Specials.Pre aEvent) {
         AbstractClientPlayer aPlayer = (AbstractClientPlayer) aEvent.entityPlayer;
         if (GT_Utility.getFullInvisibility(aPlayer)) {
             aEvent.setCanceled(true);
@@ -49,36 +55,34 @@ public class GT_CapeRenderer
             if ((tResource != null) && (!aPlayer.getHideCape())) {
                 bindTexture(tResource);
                 GL11.glPushMatrix();
-                GL11.glTranslatef(glf0, glf0, 0.125F);
-                float var1f = (float) (aPlayer.prevPosX + (aPlayer.posX - aPlayer.prevPosX) * aPartialTicks);
-                float d0 = (float) aPlayer.field_71091_bM + var1f;
-                float d1 = (float) aPlayer.field_71096_bN + var1f;
-                float d2 = (float) aPlayer.field_71097_bO + var1f;
-                float f6 = (float) (aPlayer.prevRenderYawOffset + (aPlayer.renderYawOffset - aPlayer.prevRenderYawOffset) * aPartialTicks);
-                float var2f = f6 * 3.141593F / 180.0F;
-                float d3 = MathHelper.sin(var2f);
-                float d4 = -MathHelper.cos(var2f);
-                float f7 = d1 * 10.0F;
-                float f8 = (d0 * d3 + d2 * d4) * 100.0F;
-                float f9 = (d0 * d4 - d2 * d3) * 100.0F;
+                GL11.glTranslatef(0.0F, 0.0F, 0.125F);
+                double d0 = aPlayer.field_71091_bM + (aPlayer.field_71094_bP - aPlayer.field_71091_bM) * aPartialTicks - (aPlayer.prevPosX + (aPlayer.posX - aPlayer.prevPosX) * aPartialTicks);
+                double d1 = aPlayer.field_71096_bN + (aPlayer.field_71095_bQ - aPlayer.field_71096_bN) * aPartialTicks - (aPlayer.prevPosY + (aPlayer.posY - aPlayer.prevPosY) * aPartialTicks);
+                double d2 = aPlayer.field_71097_bO + (aPlayer.field_71085_bR - aPlayer.field_71097_bO) * aPartialTicks - (aPlayer.prevPosZ + (aPlayer.posZ - aPlayer.prevPosZ) * aPartialTicks);
+                float f6 = aPlayer.prevRenderYawOffset + (aPlayer.renderYawOffset - aPlayer.prevRenderYawOffset) * aPartialTicks;
+                double d3 = MathHelper.sin(f6 * 3.141593F / 180.0F);
+                double d4 = -MathHelper.cos(f6 * 3.141593F / 180.0F);
+                float f7 = (float) d1 * 10.0F;
+                float f8 = (float) (d0 * d3 + d2 * d4) * 100.0F;
+                float f9 = (float) (d0 * d4 - d2 * d3) * 100.0F;
                 if (f7 < -6.0F) {
                     f7 = -6.0F;
                 }
                 if (f7 > 32.0F) {
                     f7 = 32.0F;
                 }
-                if (f8 < glf0) {
-                    f8 = glf0;
+                if (f8 < 0.0F) {
+                    f8 = 0.0F;
                 }
                 float f10 = aPlayer.prevCameraYaw + (aPlayer.cameraYaw - aPlayer.prevCameraYaw) * aPartialTicks;
                 f7 += MathHelper.sin((aPlayer.prevDistanceWalkedModified + (aPlayer.distanceWalkedModified - aPlayer.prevDistanceWalkedModified) * aPartialTicks) * 6.0F) * 32.0F * f10;
                 if (aPlayer.isSneaking()) {
                     f7 += 25.0F;
                 }
-                GL11.glRotatef(6.0F + f8 / 2.0F + f7, glf1, glf0, glf0);
-                GL11.glRotatef(f9 / 2.0F, glf0, glf0, glf1);
-                GL11.glRotatef(-f9 / 2.0F, glf0, glf1, glf0);
-                GL11.glRotatef(180.0F, glf0, glf1, glf0);
+                GL11.glRotatef(6.0F + f8 / 2.0F + f7, 1.0F, 0.0F, 0.0F);
+                GL11.glRotatef(f9 / 2.0F, 0.0F, 0.0F, 1.0F);
+                GL11.glRotatef(-f9 / 2.0F, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(180.0F, 0.0F, 1.0F, 0.0F);
                 ((ModelBiped) this.mainModel).renderCloak(0.0625F);
                 GL11.glPopMatrix();
             }
@@ -87,5 +91,5 @@ public class GT_CapeRenderer
                 e.printStackTrace(GT_Log.err);
             }
         }
-    }*/
+    }
 }
