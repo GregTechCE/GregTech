6dd3ab8cced8106ad90f947d36c7ed9e6e378889
Blood-Asp <bloodasphendrik@gmail.com>
2016-10-21 23:51:48 +0200

    Fix mercury crafting exploit

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingNugget.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingNugget.java
@@ -22,6 +22,7 @@ public class ProcessingNugget implements gregtech.api.interfaces.IOreRecipeRegis
         GT_RecipeRegistrator.registerReverseMacerating(aStack, aMaterial, aPrefix.mMaterialAmount, null, null, null, false);
         if (!aMaterial.contains(SubTag.NO_SMELTING)) {
             GT_Values.RA.addAlloySmelterRecipe(GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial, 1L), ItemList.Shape_Mold_Nugget.get(0L, new Object[0]), GT_Utility.copyAmount(9L, new Object[]{aStack}), 100, 1);
+            if(aMaterial==Materials.Mercury)return;
             if ((GT_ModHandler.getSmeltingOutput(GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial, 1L), false, null) == null) && (GT_OreDictUnificator.get(OrePrefixes.nugget, aMaterial.mSmeltInto, 1L) != null) && (!GT_ModHandler.addSmeltingRecipe(GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial, 1L), GT_Utility.copyAmount(9L, new Object[]{aStack})))) {
                 GT_ModHandler.addShapelessCraftingRecipe(GT_Utility.copyAmount(9L, new Object[]{aStack}), new Object[]{aOreDictName});}
         }
