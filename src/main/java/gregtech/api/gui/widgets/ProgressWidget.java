79009183b84a0ebe220df34c13efa06c8b0fbcb0
SuperCoder7979 <k.pranav@gmail.com>
2018-01-11 20:27:07 -0500

    oops

--- src/main/java/gregtech/api/gui/widgets/ProgressWidget.java
+++ src/main/java/gregtech/api/gui/widgets/ProgressWidget.java
@@ -17,30 +17,56 @@ public class ProgressWidget<T extends IWorkable & IUIHolder> extends Widget<T> {
     protected final int yPosition;
 
     protected final boolean vertical;
+    protected final boolean direction; //false = down to up , true = up to down
 
     protected float progress;
 
     private ResourceLocation onImageLocation;
     private ResourceLocation offImageLocation;
 
-    public ProgressWidget(int xPosition, int yPosition) {
-        this(xPosition, yPosition, false);
-    }
+    private int width;
+    private int height;
 
+    private int u = 0;
+    private int v = 0;
+
+    @Deprecated
+    //For people who want to quickly setup ProcessWidgets, don't use this for actual build
+    public ProgressWidget(int xPosition, int yPosition) {
+        this(xPosition, yPosition, false, false);
+    }
+    //This is okay but the next constructor is preferred
     public ProgressWidget(int xPosition, int yPosition, boolean vertical) {
+        this(xPosition, yPosition, vertical, false);
+    }
+    // This constructor allows the user to have full control over the ProcessWidget
+    public ProgressWidget(int xPosition, int yPosition, boolean vertical, boolean direction) {
         super(SLOT_DRAW_PRIORITY - 200);
         this.xPosition = xPosition;
         this.yPosition = yPosition;
-        this.vertical = vertical; //TODO vertical ProgressWidgets
+        this.vertical = vertical;
+        this.direction = direction;
     }
 
-    public ProgressWidget<T> setFilledImageLocation(ResourceLocation onImageLocation) {
+    public ProgressWidget<T> setFilledImageLocation(ResourceLocation onImageLocation) {//Filled progress bar texture location
         this.onImageLocation = onImageLocation;
         return this;
     }
 
-    public ProgressWidget<T> setImageLocation(ResourceLocation offImageLocation) {
+    public ProgressWidget<T> setImageLocation(ResourceLocation offImageLocation) {//Empty progress bar texture location
         this.offImageLocation = offImageLocation;
+        return this;
+    }
+    //This is required and minecraft will crash without it
+    public ProgressWidget<T> setImageWidthHeight(int width, int height) {
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+    //This is optional as u and v are initialized to 0
+    public ProgressWidget<T> setImageUV(int u, int v) {
+        this.u = u;
+        this.v = v;
         return this;
     }
 
@@ -48,7 +74,7 @@ public class ProgressWidget<T extends IWorkable & IUIHolder> extends Widget<T> {
     public void initWidget() {
     }
 
-    protected int getProgressScaled(int pixels) {
+    protected int getProgressScaled(int pixels) { //Find the amount of pixels to increment the filled texture by
         int maxProgress = this.gui.holder.getMaxProgress();
         return maxProgress != 0 && progress != 0 ? ((int) progress) * pixels / maxProgress : 0;
     }
@@ -58,16 +84,32 @@ public class ProgressWidget<T extends IWorkable & IUIHolder> extends Widget<T> {
     public void drawInBackground(int guiLeft, int guiTop, float partialTicks, int mouseX, int mouseY) {
         drawInBackgroundInternal(guiLeft, guiTop, () -> {
             TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
-            textureManager.bindTexture(offImageLocation);
-            Gui.drawModalRectWithCustomSizedTexture(xPosition, yPosition, 0, 0, 20, 16, 20, 16);
+            if (this.vertical){ //if it is vertical then reverse textures
+                textureManager.bindTexture(onImageLocation);
+            } else {
+                textureManager.bindTexture(offImageLocation);
+            }
+            Gui.drawModalRectWithCustomSizedTexture(xPosition, yPosition, u, v, width, height, width, height);
 
-            textureManager.bindTexture(onImageLocation);
+            if (this.vertical){//if it is vertical then reverse textures
+                textureManager.bindTexture(offImageLocation);
+            } else {
+                textureManager.bindTexture(onImageLocation);
+            }
+
             if (progress >= this.gui.holder.getMaxProgress()) {
                 progress = 0;
             } else {
                 progress += partialTicks;
             }
-            Gui.drawModalRectWithCustomSizedTexture(xPosition, yPosition, 0, 0, this.getProgressScaled(20), 16, 20, 16);
+
+            if (!this.vertical) {
+                Gui.drawModalRectWithCustomSizedTexture(xPosition, yPosition, u, v, this.getProgressScaled(width), height, width, height);
+            } else if (!direction) {
+                Gui.drawModalRectWithCustomSizedTexture(xPosition, yPosition, u, v, width, height - this.getProgressScaled(height), width, height);
+            } else {
+                Gui.drawModalRectWithCustomSizedTexture(xPosition, yPosition, u, v, width, this.getProgressScaled(height), width, height);
+            }
         });
     }
 
@@ -87,3 +129,5 @@ public class ProgressWidget<T extends IWorkable & IUIHolder> extends Widget<T> {
     public void readUpdateInfo(PacketBuffer buffer) {
     }
 }
+
+
