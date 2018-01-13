265bc347b9d68fdc336bf177f6090200c4111215
SuperCoder7979 <k.pranav@gmail.com>
2018-01-13 11:21:11 -0500

    Add steam extractor, compressor, and macerator

--- src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
+++ src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
@@ -2,7 +2,6 @@ package gregtech.common.metatileentities;
 
 import gregtech.api.GregTechAPI;
 import gregtech.api.metatileentity.GregtechTileEntity;
-import gregtech.api.metatileentity.factory.MetaTileEntityFactory;
 import gregtech.api.metatileentity.factory.TieredMetaTileEntityFactory;
 import gregtech.api.metatileentity.factory.WorkableMetaTileEntityFactory;
 import gregtech.api.recipes.RecipeMap;
@@ -10,6 +9,9 @@ import gregtech.api.util.GTResourceLocation;
 import gregtech.common.blocks.BlockMachine;
 import gregtech.common.blocks.properties.PropertyString;
 import gregtech.common.blocks.tileentity.TileEntityCableEmitter;
+import gregtech.common.metatileentities.steam.SteamCompressor;
+import gregtech.common.metatileentities.steam.SteamExtractor;
+import gregtech.common.metatileentities.steam.SteamMacerator;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class MetaTileEntities {
@@ -22,6 +24,9 @@ public class MetaTileEntities {
 
         GregTechAPI.METATILEENTITY_REGISTRY.register(1, "mte_test", new WorkableMetaTileEntityFactory<>(BlockMachine.ToolClass.WRENCH, 1, new String[]{"test_desc"}, TestMTE.class, new GTResourceLocation("mte_test"), 1, RecipeMap.FURNACE_RECIPES));
         GregTechAPI.METATILEENTITY_REGISTRY.register(2, "mte_test_generator", new TieredMetaTileEntityFactory<>(BlockMachine.ToolClass.WRENCH, 1, new String[]{"test_desc_generator"}, TestGeneratorMTE.class, new GTResourceLocation("mte_test_generator"), 1));
+        GregTechAPI.METATILEENTITY_REGISTRY.register(3, "steam_extractor", new WorkableMetaTileEntityFactory<>(BlockMachine.ToolClass.WRENCH, 1, new String[]{"Extractoring the good stuff"}, SteamExtractor.class, new GTResourceLocation("steam_extractor"), 1,RecipeMap.EXTRACTOR_RECIPES));
+        GregTechAPI.METATILEENTITY_REGISTRY.register(4, "steam_macerator", new WorkableMetaTileEntityFactory<>(BlockMachine.ToolClass.WRENCH, 1, new String[]{"Don't stick your fingers in"}, SteamMacerator.class, new GTResourceLocation("steam_macerator"), 1,RecipeMap.MACERATOR_RECIPES));
+        GregTechAPI.METATILEENTITY_REGISTRY.register(5, "steam_compressor", new WorkableMetaTileEntityFactory<>(BlockMachine.ToolClass.WRENCH, 1, new String[]{"Squeeze it together"}, SteamCompressor.class, new GTResourceLocation("steam_compressor"), 1,RecipeMap.COMPRESSOR_RECIPES));
 
         BlockMachine.META_TYPE = PropertyString.create("meta_type", GregTechAPI.METATILEENTITY_REGISTRY.getKeys());
     }
