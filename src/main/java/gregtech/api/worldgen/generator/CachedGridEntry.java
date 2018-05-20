138bdbe51e9c79f5adda403f2657e95fa77dee56
Archengius <Archengius@gmail.com>
2018-05-20 21:26:04 +0700

    some changes to generator, should work much faster now

---
+++ src/main/java/gregtech/api/worldgen/generator/CachedGridEntry.java
@@ -0,0 +1,205 @@
+package gregtech.api.worldgen.generator;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import gnu.trove.list.TLongList;
+import gnu.trove.list.array.TLongArrayList;
+import gnu.trove.map.TLongObjectMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
+import gregtech.api.util.GTUtility;
+import gregtech.api.util.XSTR;
+import gregtech.api.worldgen.config.OreDepositDefinition;
+import gregtech.api.worldgen.config.WorldGenRegistry;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+
+public class CachedGridEntry implements IBlockGeneratorAccess {
+
+    private static final Map<World, LoadingCache<Long, CachedGridEntry>> gridEntryCache = new WeakHashMap<>();
+
+    public static CachedGridEntry getOrCreateEntry(World world, int gridX, int gridZ) {
+        LoadingCache<Long, CachedGridEntry> currentValue = gridEntryCache.get(world);
+        if(currentValue == null) {
+            currentValue = createGridCache(world);
+            gridEntryCache.put(world, currentValue);
+        }
+        Long gridEntryKey = (long) gridX << 32 | gridZ & 0xFFFFFFFFL;
+        return currentValue.getUnchecked(gridEntryKey);
+    }
+
+    private static LoadingCache<Long, CachedGridEntry> createGridCache(World world) {
+        return CacheBuilder.newBuilder()
+            .maximumSize(300)
+            .expireAfterAccess(5L, TimeUnit.MINUTES)
+            .build(new CacheLoader<Long, CachedGridEntry>() {
+                @Override
+                public CachedGridEntry load(Long key) {
+                    long actualKey = key;
+                    int gridX = (int) (actualKey >> 32);
+                    int gridZ = (int) actualKey;
+                    return new CachedGridEntry(world, gridX, gridZ);
+                }
+            });
+    }
+
+    private final TLongObjectMap<ChunkDataEntry> dataByChunkPos = new TLongObjectHashMap<>();
+
+    private final XSTR gridRandom;
+    private final int gridX;
+    private final int gridZ;
+    private List<Entry<Integer, OreDepositDefinition>> cachedDepositMap;
+    private int maxHeight;
+    private List<OreDepositDefinition> generatedVeins;
+
+    private int veinCenterX, veinCenterY, veinCenterZ;
+    private OreDepositDefinition currentOreVein;
+
+    public CachedGridEntry(World world, int gridX, int gridZ) {
+        this.gridX = gridX;
+        this.gridZ = gridZ;
+        long gridRandomSeed = Objects.hash(gridX, gridZ) ^ world.getSeed();
+        this.gridRandom = new XSTR(gridRandomSeed);
+
+        int gridSizeX = WorldGeneratorImpl.GRID_SIZE_X * 16;
+        int gridSizeZ = WorldGeneratorImpl.GRID_SIZE_Z * 16;
+        BlockPos blockPos = new BlockPos(gridX * gridSizeX + gridSizeX / 2, world.getActualHeight(), gridZ * gridSizeZ + gridSizeZ / 2);
+        Biome currentBiome = world.getBiomeProvider().getBiome(blockPos);
+        this.cachedDepositMap = new ArrayList<>(WorldGenRegistry.INSTANCE.getCachedBiomeVeins(world.provider, currentBiome));
+        this.maxHeight = world.getActualHeight();
+        this.generatedVeins = triggerVeinsGeneration();
+    }
+
+    public List<OreDepositDefinition> getGeneratedVeins() {
+        return Collections.unmodifiableList(generatedVeins);
+    }
+
+    public void populateChunk(World world, int chunkX, int chunkZ) {
+        long chunkId = (long) chunkX << 32 | chunkZ & 0xFFFFFFFFL;
+        ChunkDataEntry chunkDataEntry = dataByChunkPos.get(chunkId);
+        if(chunkDataEntry != null) {
+            chunkDataEntry.populateChunk(world);
+        }
+    }
+
+    public List<OreDepositDefinition> triggerVeinsGeneration() {
+        if(cachedDepositMap.isEmpty())
+            return Collections.emptyList(); //do not try to generate an empty vein list
+
+        ArrayList<OreDepositDefinition> generatedDeposits = new ArrayList<>();
+        int currentCycle = 0;
+        int maxCycles = 2 + gridRandom.next(5);
+        while(currentCycle < cachedDepositMap.size() && currentCycle < maxCycles) {
+            //instead of removing already generated veins, we swap last element with one we selected
+            int randomEntryIndex = GTUtility.getRandomItem(gridRandom, cachedDepositMap, cachedDepositMap.size() - currentCycle);
+            OreDepositDefinition randomEntry = cachedDepositMap.get(randomEntryIndex).getValue();
+            Collections.swap(cachedDepositMap, randomEntryIndex, cachedDepositMap.size() - 1 - currentCycle);
+            //need to put into list first to apply priority properly, so
+            //red granite vein will be properly filled with ores from other veins
+            generatedDeposits.add(randomEntry);
+            currentCycle++;
+        }
+        //sort generated veins according to their priority, so they get mixed in properly
+        generatedDeposits.sort(Collections.reverseOrder(Comparator.comparing(OreDepositDefinition::getPriority)));
+        //and finally generate all of them
+        for(OreDepositDefinition depositDefinition : generatedDeposits) {
+            doGenerateVein(depositDefinition);
+        }
+        return generatedDeposits;
+    }
+
+    private void doGenerateVein(OreDepositDefinition definition) {
+        this.currentOreVein = definition;
+        int gridSizeX = WorldGeneratorImpl.GRID_SIZE_X * 16;
+        int gridSizeZ = WorldGeneratorImpl.GRID_SIZE_Z * 16;
+        this.veinCenterX = gridX * gridSizeX + gridRandom.nextInt(gridSizeX);
+        this.veinCenterY = gridRandom.nextInt(maxHeight);
+        this.veinCenterZ = gridZ * gridSizeZ + gridRandom.nextInt(gridSizeZ);
+        this.currentOreVein.getShapeGenerator().generate(new XSTR(gridRandom.getSeed()), this);
+        this.currentOreVein = null;
+    }
+
+    @Override
+    public boolean generateBlock(int x, int y, int z) {
+        if(currentOreVein == null)
+            throw new IllegalStateException("Attempted to call generateBlock without current ore vein!");
+        int globalBlockX = veinCenterX + x;
+        int globalBlockY = veinCenterY + y;
+        int globalBlockZ = veinCenterZ + z;
+        //we should do all random-related things here, otherwise it gets corrupted by current chunk information
+        float randomDensityValue = gridRandom.nextFloat();
+
+        if(currentOreVein.getDensity() < randomDensityValue)
+            return false; //only place blocks in positions matching density
+
+        setBlock(globalBlockX, globalBlockY, globalBlockZ, currentOreVein);
+        return true;
+    }
+
+    private void setBlock(int worldX, int worldY, int worldZ, OreDepositDefinition definition) {
+        int chunkX = worldX >> 4;
+        int chunkZ = worldZ >> 4;
+        int localX = worldX - chunkX * 16;
+        int localZ = worldZ - chunkZ * 16;
+        long chunkKey = (long) chunkX << 32 | chunkZ & 0xFFFFFFFFL;
+        ChunkDataEntry dataEntry = dataByChunkPos.get(chunkKey);
+        if(dataEntry == null) {
+            dataEntry = new ChunkDataEntry(chunkX, chunkZ);
+            dataByChunkPos.put(chunkKey, dataEntry);
+        }
+        dataEntry.setBlock(localX, worldY, localZ, definition);
+    }
+
+
+    public static class ChunkDataEntry {
+
+        private final Map<OreDepositDefinition, TLongList> oreBlocks = new HashMap<>();
+        private final int chunkX;
+        private final int chunkZ;
+
+        public ChunkDataEntry(int chunkX, int chunkZ) {
+            this.chunkX = chunkX;
+            this.chunkZ = chunkZ;
+        }
+
+        public void setBlock(int x, int y, int z, OreDepositDefinition definition) {
+            int xzValue = ((x & 0xFF) << 8) | (z & 0xFF);
+            long blockIndex = (long) xzValue << 32 | y & 0xFFFFFFFFL;
+            TLongList longList = oreBlocks.get(definition);
+            if(longList == null) {
+                longList = new TLongArrayList();
+                oreBlocks.put(definition, longList);
+            }
+            longList.add(blockIndex);
+        }
+
+        public void populateChunk(World world) {
+            MutableBlockPos blockPos = new MutableBlockPos();
+            for(OreDepositDefinition definition : oreBlocks.keySet()) {
+                long[] blockIndexArray = oreBlocks.get(definition).toArray();
+                for(long blockIndex : blockIndexArray) {
+                    int xzValue = (int) (blockIndex >> 32);
+                    int blockY = (int) blockIndex;
+                    int blockX = (byte) (xzValue >> 8);
+                    int blockZ = (byte) xzValue;
+                    blockPos.setPos(chunkX * 16 + blockX, blockY, chunkZ * 16 + blockZ);
+                    IBlockState currentState = world.getBlockState(blockPos);
+                    if(!definition.getGenerationPredicate().test(currentState))
+                        continue; //do not generate if predicate didn't match
+                    IBlockState newState = definition.getBlockFiller().getStateForGeneration(currentState, blockX, blockY, blockZ);
+                    //set flags as 16 to avoid observer updates loading neighbour chunks
+                    world.setBlockState(blockPos, newState, 16);
+                }
+            }
+        }
+
+    }
+
+}
--- src/main/java/gregtech/api/worldgen/generator/WorldGeneratorImpl.java
+++ src/main/java/gregtech/api/worldgen/generator/WorldGeneratorImpl.java
@@ -14,10 +14,7 @@ import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.fluids.IFluidBlock;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 //TODO implement CC support here
 public class WorldGeneratorImpl implements IWorldGenerator {
@@ -30,52 +27,53 @@ public class WorldGeneratorImpl implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         int selfGridX = Math.floorDiv(chunkX, GRID_SIZE_X);
         int selfGridZ = Math.floorDiv(chunkZ, GRID_SIZE_Z);
-        //because some way to generate random Y is needed here
-        int randomChunkY = (int) ((Objects.hash(selfGridX, selfGridZ) ^ world.getSeed()) % 16);
-        int selfGridY = Math.floorDiv(randomChunkY, GRID_SIZE_Y);
-        List<OreDepositDefinition> generatedOres = generateInternal(world, selfGridX, selfGridY, selfGridZ, chunkX, randomChunkY, chunkZ, false);
+        List<OreDepositDefinition> generatedOres = generateInternal(world, selfGridX, selfGridZ, chunkX, chunkZ);
+
         //if generated ores aren't empty, and surface rocks are enabled in config, attempt to generate them
-        if(!generatedOres.isEmpty() && ConfigHolder.enableOreVeinSurfaceRocks) {
-            for(OreDepositDefinition depositDefinition : generatedOres) {
-                MetalMaterial material = depositDefinition.getSurfaceStoneMaterial();
-                if(material == null) continue;
-                int stonesCount = random.nextInt(2);
-                for(int i = 0; i < stonesCount; i++) {
-                    int randomX = chunkX * 16 + random.nextInt(16);
-                    int randomZ = chunkZ * 16 + random.nextInt(16);
-                    BlockPos topBlockPos = new BlockPos(randomX, 0, randomZ);
-                    topBlockPos = world.getTopSolidOrLiquidBlock(topBlockPos).down();
-                    IBlockState blockState = world.getBlockState(topBlockPos);
-                    if(!blockState.isBlockNormalCube() || !blockState.isFullBlock())
-                        continue;
-                    BlockPos topBlock = topBlockPos.up();
-                    IBlockState upperState = world.getBlockState(topBlockPos);
-                    if(upperState.getBlock() instanceof BlockLiquid ||
-                        upperState.getBlock() instanceof IFluidBlock)
-                        continue; //do not try to generate inside fluid blocks
-                    BlockSurfaceRock blockSurfaceRock = MetaBlocks.SURFACE_ROCKS.get(material);
-                    IBlockState statePlace = blockSurfaceRock.getDefaultState().withProperty(blockSurfaceRock.materialProperty, material);
-                    world.setBlockState(topBlock, statePlace, 16);
-                }
+        if (generatedOres.isEmpty() || !ConfigHolder.enableOreVeinSurfaceRocks)
+            return;
+
+        for (OreDepositDefinition depositDefinition : generatedOres) {
+            MetalMaterial material = depositDefinition.getSurfaceStoneMaterial();
+            if (material == null) continue;
+            int stonesCount = random.nextInt(2);
+
+            for (int i = 0; i < stonesCount; i++) {
+                int randomX = chunkX * 16 + random.nextInt(16);
+                int randomZ = chunkZ * 16 + random.nextInt(16);
+                BlockPos topBlockPos = new BlockPos(randomX, 0, randomZ);
+                topBlockPos = world.getTopSolidOrLiquidBlock(topBlockPos).down();
+                IBlockState blockState = world.getBlockState(topBlockPos);
+                if (!blockState.isBlockNormalCube() || !blockState.isFullBlock())
+                    continue; //do not generate on non-solid blocks
+
+                BlockPos topBlock = topBlockPos.up();
+                IBlockState upperState = world.getBlockState(topBlockPos);
+                if (upperState.getBlock() instanceof BlockLiquid ||
+                    upperState.getBlock() instanceof IFluidBlock)
+                    continue; //do not try to generate inside fluid blocks
+
+                BlockSurfaceRock blockSurfaceRock = MetaBlocks.SURFACE_ROCKS.get(material);
+                IBlockState statePlace = blockSurfaceRock.getDefaultState().withProperty(blockSurfaceRock.materialProperty, material);
+                world.setBlockState(topBlock, statePlace, 16);
             }
         }
     }
 
-    private List<OreDepositDefinition> generateInternal(World world, int selfGridX, int selfGridY, int selfGridZ, int chunkX, int chunkY, int chunkZ, boolean respectYChunk) {
-        GeneratorAccessImpl generatorAccess = new GeneratorAccessImpl(world, chunkX, chunkY, chunkZ, respectYChunk);
-        ArrayList<OreDepositDefinition> allGeneratedOres = new ArrayList<>();
+    private List<OreDepositDefinition> generateInternal(World world, int selfGridX, int selfGridZ, int chunkX, int chunkZ) {
+        List<OreDepositDefinition> allGeneratedOres = Collections.emptyList();
         int halfSizeX = (GRID_SIZE_X - 1) / 2;
         int halfSizeY = (GRID_SIZE_Y - 1) / 2;
         int halfSizeZ = (GRID_SIZE_Z - 1) / 2;
         for(int gridX = -halfSizeX; gridX <= halfSizeX; gridX++) {
-            for(int gridY = -halfSizeY; gridY <= halfSizeY; gridY++) {
                 for(int gridZ = -halfSizeZ; gridZ <= halfSizeZ; gridZ++) {
-                    if(!respectYChunk && (selfGridY + gridY < 0 || selfGridY + gridY > 5))
-                        continue; //if not respecting y chunks, then skip generation outside vanilla range
-                    generatorAccess.setupGridEntry(selfGridX + gridX, selfGridY + gridY, selfGridZ + gridZ);
-                    allGeneratedOres.addAll(generatorAccess.triggerVeinsGeneration());
+                    CachedGridEntry cachedGridEntry = CachedGridEntry.getOrCreateEntry(world, selfGridX + gridX, selfGridZ + gridZ);
+                    cachedGridEntry.populateChunk(world, chunkX, chunkZ);
+                    if(gridX == 0 && gridZ == 0) {
+                        //add generated definitions only in current grid entry
+                        allGeneratedOres = cachedGridEntry.getGeneratedVeins();
+                    }
                 }
-            }
         }
         return allGeneratedOres;
     }
