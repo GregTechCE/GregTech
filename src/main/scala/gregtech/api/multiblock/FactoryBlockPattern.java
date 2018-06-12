042dd564df77d1f6dfb213d653d9fba43da3ffbc
Antifluxfield <lyj_299792458@163.com>
2018-06-12 11:41:42 +0800

    Better multiblock pattern

--- src/main/scala/gregtech/api/multiblock/FactoryBlockPattern.java
+++ src/main/scala/gregtech/api/multiblock/FactoryBlockPattern.java
@@ -5,30 +5,46 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 import java.util.function.Predicate;
+
+import gregtech.api.multiblock.BlockPattern.RelativeDirection;
+import static gregtech.api.multiblock.BlockPattern.RelativeDirection.*;
 
 public class FactoryBlockPattern {
 
     private static final Joiner COMMA_JOIN = Joiner.on(",");
     private final List<String[]> depth = new ArrayList<>();
+    private final List<int[]> aisleRepetitions = new ArrayList<>();
     private final Map<Character, Predicate<BlockWorldState>> symbolMap = new HashMap<>();
     private int aisleHeight;
     private int rowWidth;
+    private RelativeDirection[] structureDir = new RelativeDirection[3];
 
-    private FactoryBlockPattern() {
+    private FactoryBlockPattern(RelativeDirection charDir, RelativeDirection stringDir, RelativeDirection aisleDir) {
+        structureDir[0] = charDir;
+        structureDir[1] = stringDir;
+        structureDir[2] = aisleDir;
+        int flags = 0;
+        for (int i = 0; i < 3; i++) {
+            switch (structureDir[i]) {
+                case UP:
+                case DOWN: flags |= 0x1; break;
+                case LEFT:
+                case RIGHT: flags |= 0x2; break;
+                case FRONT:
+                case BACK: flags |= 0x4; break;
+            }
+        }
+        if (flags != 0x7) throw new IllegalArgumentException("Must have 3 different axes!");
         this.symbolMap.put(' ', k -> true);
     }
 
     /**
-     * Adds a single aisle to this pattern, going in the z axis. (so multiple calls to this will increase the z-size by
-     * 1)
+     * Adds a repeatable aisle to this pattern.
      */
-    public FactoryBlockPattern aisle(String... aisle) {
+    public FactoryBlockPattern aisleRepeatable(int minRepeat, int maxRepeat, String... aisle) {
         if (!ArrayUtils.isEmpty(aisle) && !StringUtils.isEmpty(aisle[0])) {
             if (this.depth.isEmpty()) {
                 this.aisleHeight = aisle.length;
@@ -51,6 +67,8 @@ public class FactoryBlockPattern {
                 }
 
                 this.depth.add(aisle);
+                if (minRepeat > maxRepeat) throw new IllegalArgumentException("Lower bound of repeat counting must smaller than upper bound!");
+                aisleRepetitions.add(new int[]{minRepeat, maxRepeat});
                 return this;
             }
         } else {
@@ -58,8 +76,35 @@ public class FactoryBlockPattern {
         }
     }
 
+    /**
+     * Adds a single aisle to this pattern. (so multiple calls to this will increase the aisleDir by 1)
+     */
+    public FactoryBlockPattern aisle(String... aisle) {
+        return aisleRepeatable(1, 1, aisle);
+    }
+
+    /**
+     * Set last aisle repeatable
+     */
+    public FactoryBlockPattern setRepeatable(int minRepeat, int maxRepeat) {
+        if (minRepeat > maxRepeat) throw new IllegalArgumentException("Lower bound of repeat counting must smaller than upper bound!");
+        aisleRepetitions.set(aisleRepetitions.size() - 1, new int[]{minRepeat, maxRepeat});
+        return this;
+    }
+
+    /**
+     * Set last aisle repeatable
+     */
+    public FactoryBlockPattern setRepeatable(int repeatCount) {
+        return setRepeatable(repeatCount, repeatCount);
+    }
+
     public static FactoryBlockPattern start() {
-        return new FactoryBlockPattern();
+        return new FactoryBlockPattern(RIGHT, UP, BACK);
+    }
+
+    public static FactoryBlockPattern start(RelativeDirection charDir, RelativeDirection stringDir, RelativeDirection aisleDir) {
+        return new FactoryBlockPattern(charDir, stringDir, aisleDir);
     }
 
     public FactoryBlockPattern where(char symbol, Predicate<BlockWorldState> blockMatcher) {
@@ -68,7 +113,7 @@ public class FactoryBlockPattern {
     }
 
     public BlockPattern build() {
-        return new BlockPattern(this.makePredicateArray());
+        return new BlockPattern(this.makePredicateArray(), structureDir, aisleRepetitions.toArray(new int[aisleRepetitions.size()][]));
     }
 
     @SuppressWarnings("unchecked")
