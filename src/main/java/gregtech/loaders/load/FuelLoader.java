9f69831e232fbc463a04094a4998cc9a0a86c075
pyure <pyure@hotmail.com>
2018-07-01 15:19:43 -0400

    Fix fluid generators to output tier-appropriate voltage (#188)
    * Cobalt now generates rods
    
    * tweaked fluid generators to output tier-rated voltage

--- src/main/java/gregtech/loaders/load/FuelLoader.java
+++ src/main/java/gregtech/loaders/load/FuelLoader.java
@@ -8,107 +8,108 @@ public class FuelLoader {
     public static void registerFuels() {
         //128000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.Fuel.getFluid(1))
-            .duration(4)
+            .fluidInputs(Materials.Fuel.getFluid(12))
+            .duration(48)
             .EUt(-32)
             .buildAndRegister();
 
-        //6000EU
+        //6400EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.BioFuel.getFluid(1))
-            .duration(1)
-            .EUt(-6)
+            .fluidInputs(Materials.BioFuel.getFluid(60))
+            .duration(12)
+            .EUt(-32)
             .buildAndRegister();
 
         //32000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.SulfuricLightFuel.getFluid(1))
-            .duration(2)
-            .EUt(-16)
+            .fluidInputs(Materials.SulfuricLightFuel.getFluid(12))
+            .duration(12)
+            .EUt(-32)
             .buildAndRegister();
 
         //32000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.SulfuricNaphtha.getFluid(1))
-            .duration(2)
-            .EUt(-16)
+            .fluidInputs(Materials.SulfuricNaphtha.getFluid(12))
+            .duration(12)
+            .EUt(-32)
             .buildAndRegister();
 
         //256000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.SulfuricHeavyFuel.getFluid(1))
-            .duration(4)
-            .EUt(-64)
+            .fluidInputs(Materials.SulfuricHeavyFuel.getFluid(12))
+            .duration(24)
+            .EUt(-128)
             .buildAndRegister();
 
         //148000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.Ethanol.getFluid(4))
-            .duration(4)
-            .EUt(-148)
+            .fluidInputs(Materials.Ethanol.getFluid(48))
+            .duration(14)
+            .EUt(-512)
             .buildAndRegister();
 
         //256000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.LightFuel.getFluid(1))
-            .duration(8)
+            .fluidInputs(Materials.LightFuel.getFluid(12))
+            .duration(96)
             .EUt(-32)
             .buildAndRegister();
 
         //256000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.Naphtha.getFluid(1))
-            .duration(2)
-            .EUt(-128)
+            .fluidInputs(Materials.Naphtha.getFluid(12))
+            .duration(96)
+            .EUt(-32)
             .buildAndRegister();
 
         //192000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.HeavyFuel.getFluid(1))
-            .duration(1)
-            .EUt(-192)
+            .fluidInputs(Materials.HeavyFuel.getFluid(13))
+            .duration(5)
+            .EUt(-512)
             .buildAndRegister();
 
         //512000EU
         RecipeMaps.DIESEL_GENERATOR_FUELS.recipeBuilder()
-            .fluidInputs(Materials.NitroFuel.getFluid(1))
-            .duration(1)
+            .fluidInputs(Materials.NitroFuel.getFluid(12))
+            .duration(12)
             .EUt(-512)
             .buildAndRegister();
 
         //8 SteamUnit -> 5EU/t
+        //52 SteamUnit -> 32EU/t
         RecipeMaps.STEAM_TURBINE_FUELS.recipeBuilder()
-            .fluidInputs(Materials.Steam.getFluid(8))
-            .duration(1)
-            .EUt(-5)
+            .fluidInputs(Materials.Steam.getFluid(624))
+            .duration(12)
+            .EUt(-32)
             .buildAndRegister();
 
         //20000EU
         RecipeMaps.GAS_TURBINE_FUELS.recipeBuilder()
-            .fluidInputs(Materials.Hydrogen.getFluid(10))
-            .duration(10)
-            .EUt(-20)
+            .fluidInputs(Materials.Hydrogen.getFluid(120))
+            .duration(75)
+            .EUt(-32)
             .buildAndRegister();
 
-        //208000EU
+        //208000EU  // actually seems to produce 104000
         RecipeMaps.GAS_TURBINE_FUELS.recipeBuilder()
-            .fluidInputs(Materials.Methane.getFluid(5))
-            .duration(10)
-            .EUt(-52)
+            .fluidInputs(Materials.Methane.getFluid(60))
+            .duration(49)
+            .EUt(-128)
             .buildAndRegister();
 
         //256000EU
         RecipeMaps.GAS_TURBINE_FUELS.recipeBuilder()
-            .fluidInputs(Materials.LPG.getFluid(1))
-            .duration(2)
+            .fluidInputs(Materials.LPG.getFluid(12))
+            .duration(24)
             .EUt(-128)
             .buildAndRegister();
 
-        //9600EU
+        //9600EU // actually seems to produce 72000
         RecipeMaps.GAS_TURBINE_FUELS.recipeBuilder()
-            .fluidInputs(Materials.NaturalGas.getFluid(10))
-            .duration(10)
-            .EUt(-72)
+            .fluidInputs(Materials.NaturalGas.getFluid(120))
+            .duration(68)
+            .EUt(-128)
             .buildAndRegister();
     }
 }
