b9080029cf7d153de58cf6fd8b62bdfa3d8376d3
Archengius <Archengius@gmail.com>
2018-07-03 17:14:46 +0700

    bump version

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -7,7 +7,7 @@ public final class GregTechVersion {
     //This number is incremented every minecraft release, never reset
     public static final int MINOR = 4;
     //This number is incremented every time new feature is added, and reset every Minecraft version
-    public static final int REVISION = 5;
+    public static final int REVISION = 6;
     //This number is incremented every build, and never reset. Should always be 0 in the repo code.
     public static final int BUILD = 0;
 
