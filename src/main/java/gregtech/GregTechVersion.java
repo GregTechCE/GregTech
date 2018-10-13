4ede5e1daebd9861571456a2441a34cda67b1425
Archengius <Archengius@gmail.com>
2018-10-14 00:39:29 +0700

    bump version get rid of old curseforge plugin (for now)

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -5,7 +5,7 @@ public final class GregTechVersion {
     //This number is incremented every minecraft release, never reset
     public static final int MAJOR = 0;
     //This number is incremented every major feature update, and reset every minecraft release
-    public static final int MINOR = 6;
+    public static final int MINOR = 7;
     //This number is incremented every time the feature is added, or bug is fixed. resets every minor version change
     public static final int REVISION = 0;
     //This number is incremented every build, and never reset. Should always be 0 in the repo code.
