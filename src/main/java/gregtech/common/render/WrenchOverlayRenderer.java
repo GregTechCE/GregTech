13d68e5369f5171c5db9df80ab2e570d8d319259
SuperCoder79 <k.pranav@gmail.com>
2018-10-15 17:57:31 -0400

    fixed server crash

--- src/main/java/gregtech/common/render/WrenchOverlayRenderer.java
+++ src/main/java/gregtech/common/render/WrenchOverlayRenderer.java
@@ -28,7 +28,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class WrenchOverlayRenderer {
 
     @SubscribeEvent
