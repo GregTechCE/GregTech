db08ff83eafed072abc2a38e5900d6ea7ef584bb
Pyure <pyure@hotmail.com>
2019-02-15 19:33:55 -0500

    ellipsoid generator height property

--- src/main/java/gregtech/api/worldgen/shape/EllipsoidGenerator.java
+++ src/main/java/gregtech/api/worldgen/shape/EllipsoidGenerator.java
@@ -1,5 +1,6 @@
 package gregtech.api.worldgen.shape;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import crafttweaker.annotations.ZenRegister;
 import gregtech.api.worldgen.config.OreConfigUtils;
@@ -14,6 +15,8 @@ public class EllipsoidGenerator extends ShapeGenerator {
 
     private int radiusMin;
     private int radiusMax;
+    private int heightMin;
+    private int heightMax;
 
     public EllipsoidGenerator() {
     }
@@ -27,31 +30,47 @@ public class EllipsoidGenerator extends ShapeGenerator {
 
     @Override
     public void loadFromConfig(JsonObject object) {
-        int[] data = OreConfigUtils.getIntRange(object.get("radius"));
-        this.radiusMin = data[0];
-        this.radiusMax = data[1];
+        int[] radiusData = OreConfigUtils.getIntRange(object.get("radius"));
+        this.radiusMin = radiusData[0];
+        this.radiusMax = radiusData[1];
+
+        JsonElement heightJson = object.get("height");
+        if (heightJson != null) {
+            int[] heightData = OreConfigUtils.getIntRange(heightJson);
+            if (heightData.length == 2) {
+                this.heightMin = heightData[0];
+                this.heightMax = heightData[1];
+            }
+        }
     }
 
     @Override
     public Vec3i getMaxSize() {
-        return new Vec3i(radiusMax * 2, radiusMax, radiusMax * 2);
+        return new Vec3i(radiusMax * 2, heightMax == 0 ? radiusMax : heightMax * 2, radiusMax * 2);
     }
 
     @Override
     public void generate(Random gridRandom, IBlockGeneratorAccess blockAccess) {
         int a = radiusMin >= radiusMax ? radiusMin : (gridRandom.nextInt(radiusMax - radiusMin) + radiusMin);
-        int b = radiusMin >= radiusMax ? radiusMin / 2 : (gridRandom.nextInt(radiusMax - radiusMin) + radiusMin) / 2;
-        int c = radiusMin >= radiusMax ? radiusMin : (gridRandom.nextInt(radiusMax - radiusMin) + radiusMin);
+        int b;
+        if (heightMin == 0) {
+            // default if no height prop provided (also: originally coded behavior)
+            b = radiusMin >= radiusMax ? radiusMin / 2 : (gridRandom.nextInt(radiusMax - radiusMin) + radiusMin) / 2;
+        } else {
+            b = heightMin >= heightMax ? heightMin : (gridRandom.nextInt(heightMax - heightMin) + heightMin);
+        }
+        int c = a; // somewhat pointless, but establishes pattern for other (non-symmetrical) shapes.  Ellipsoids will always be circular as viewed from above.
+
         int ab2 = a * a * b * b, ac2 = a * a * c * c, bc2 = b * b * c * c, abc2 = ab2 * c * c;
 
-        int max = Math.max(a, Math.max(b, c));
-        for (int x = -max; x <= max; x++) {
+        for (int x = -a; x <= a; x++) {
             int xr = bc2 * x * x;
-            if(xr > abc2) continue;
-            for (int y = -max; y <= max; y++) {
+
+            for (int y = -b; y <= b; y++) {
                 int yr = xr + ac2 * y * y + ab2;
                 if(yr > abc2) continue;
-                for (int z = -max; z <= max; z++) {
+
+                for (int z = -c; z <= c; z++) {
                     int zr = yr + ab2 * z * z;
                     if (zr > abc2) continue;
                     blockAccess.generateBlock(x, y, z);
