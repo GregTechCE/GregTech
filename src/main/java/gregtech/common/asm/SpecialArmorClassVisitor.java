9c80ee8573c7095e9afb68e4dc00f5f252d85720
Archengius <Archengius@gmail.com>
2019-10-13 16:59:25 +0700

    Fixed gitlab CI build script x3

--- src/main/java/gregtech/common/asm/SpecialArmorClassVisitor.java
+++ src/main/java/gregtech/common/asm/SpecialArmorClassVisitor.java
@@ -1,8 +1,8 @@
 package gregtech.common.asm;
 
-import jdk.internal.org.objectweb.asm.Opcodes;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 import java.util.function.Function;
 
