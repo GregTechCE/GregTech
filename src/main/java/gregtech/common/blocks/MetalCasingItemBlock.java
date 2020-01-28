ad615ef67dfeee9fc8881a560d8600eae8fd7993
huneau romain <huneau.romain@gmail.com>
2020-01-28 10:54:34 +0100

    revert metal_casing name

--- src/main/java/gregtech/common/blocks/MetalCasingItemBlock.java
+++ src/main/java/gregtech/common/blocks/MetalCasingItemBlock.java
@@ -1,8 +1,8 @@
 package gregtech.common.blocks;
 
 import gregtech.api.unification.material.type.Material;
-import gregtech.api.unification.ore.OrePrefix;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
@@ -32,6 +32,8 @@ public class MetalCasingItemBlock extends ItemBlock {
     @SideOnly(Side.CLIENT)
     public String getItemStackDisplayName(ItemStack stack) {
         Material material = metalCasingBlock.getMetalCasingMaterial();
-        return OrePrefix.metalCasing.getLocalNameForItem(material);
+        return I18n.format(String.format("tile.metal_casing.%s.name", material.getUnlocalizedName()));
     }
+
+
 }
