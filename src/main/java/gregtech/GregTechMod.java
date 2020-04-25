cf91b38ce339fc3033812a66f0660ecdbd503566
LAGIdiot <bordel.muj@gmail.com>
2020-04-25 11:09:40 +0200

    All dependencies were updated to latest/last official version available (last is really meant as LAST because most of them did not get update in years)
    Forge updated to 14.23.5.2847
    
    CraftTweaker updated to 4.1.9.6
    CodeChickenLib updated to 3.2.3.358
    Forestry updated to 5.8.2.387
    ForgeMultipart updated to 2.6.2.83
    JEI updated to 4.15.0.291
    The One Probe updated to 1.4.28

--- src/main/java/gregtech/GregTechMod.java
+++ src/main/java/gregtech/GregTechMod.java
@@ -49,7 +49,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @Mod(modid = GTValues.MODID,
     name = "GregTech Community Edition",
     acceptedMinecraftVersions = "[1.12,1.13)",
-    dependencies = "required:forge@[14.23.5.2838,);" + CodeChickenLib.MOD_VERSION_DEP + "after:forestry;after:forgemultipartcbe;after:jei@[4.8.6,);after:crafttweaker;")
+    dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + "after:forestry;after:forgemultipartcbe;after:jei@[4.15.0,);after:crafttweaker;")
 public class GregTechMod {
 
     static {
