245e8c106c76c008ebf1d036dd4823891f55ea69
LAGIdiot <bordel.muj@gmail.com>
2020-09-27 12:41:22 +0200

    Updated GTFluidUtils to be more readable

--- src/main/java/gregtech/api/util/GTFluidUtils.java
+++ src/main/java/gregtech/api/util/GTFluidUtils.java
@@ -15,12 +15,19 @@ public class GTFluidUtils {
 
     public static int transferFluids(@Nonnull IFluidHandler sourceHandler, @Nonnull IFluidHandler destHandler, int transferLimit, @Nonnull Predicate<FluidStack> fluidFilter) {
         int fluidLeftToTransfer = transferLimit;
+
         for (IFluidTankProperties tankProperties : sourceHandler.getTankProperties()) {
             FluidStack currentFluid = tankProperties.getContents();
-            if (currentFluid == null || currentFluid.amount == 0 || !fluidFilter.test(currentFluid)) continue;
+            if (currentFluid == null || currentFluid.amount == 0 || !fluidFilter.test(currentFluid)) {
+                continue;
+            }
+
             currentFluid.amount = fluidLeftToTransfer;
             FluidStack fluidStack = sourceHandler.drain(currentFluid, false);
-            if (fluidStack == null || fluidStack.amount == 0) continue;
+            if (fluidStack == null || fluidStack.amount == 0) {
+                continue;
+            }
+
             int canInsertAmount = destHandler.fill(fluidStack, false);
             if (canInsertAmount > 0) {
                 fluidStack.amount = canInsertAmount;
@@ -29,7 +36,9 @@ public class GTFluidUtils {
                     destHandler.fill(fluidStack, true);
 
                     fluidLeftToTransfer -= fluidStack.amount;
-                    if (fluidLeftToTransfer == 0) break;
+                    if (fluidLeftToTransfer == 0) {
+                        break;
+                    }
                 }
             }
         }
