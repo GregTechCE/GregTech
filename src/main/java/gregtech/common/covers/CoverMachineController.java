73f00ebda3583f00552a7cf9457bca474920d83d
LAGIdiot <bordel.muj@gmail.com>
2020-11-09 19:04:14 +0100

    Update src/main/java/gregtech/common/covers/CoverMachineController.java

--- src/main/java/gregtech/common/covers/CoverMachineController.java
+++ src/main/java/gregtech/common/covers/CoverMachineController.java
@@ -223,7 +223,7 @@ public class CoverMachineController extends CoverBehavior implements CoverWithUI
     public void readFromNBT(NBTTagCompound tagCompound) {
         super.readFromNBT(tagCompound);
         this.minRedstoneStrength = tagCompound.getInteger("MinRedstoneStrength");
-        if (minRedstoneStrength > 15) minRedstoneStrength = 15;
+        if (minRedstoneStrength > 15) this.minRedstoneStrength = 15;
         this.isInverted = tagCompound.getBoolean("Inverted");
         this.controllerMode = ControllerMode.values()[tagCompound.getInteger("ControllerMode")];
     }
