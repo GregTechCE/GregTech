969a971d985e1f97ec8b691c7360769ffdea2e53
ALongStringOfNumbers <31759736+ALongStringOfNumbers@users.noreply.github.com>
2020-12-14 15:40:00 -0700

    Fix Multiblocks voiding partial multi item outputs when output is mostly Full (#1337)
    Co-authored-by: Exaxxion <Exaxxion@users.noreply.github.com>

---
+++ src/main/java/gregtech/api/util/InventoryUtils.java
@@ -0,0 +1,118 @@
+package gregtech.api.util;
+
+import net.minecraft.item.*;
+import net.minecraftforge.items.*;
+
+import java.util.*;
+
+public class InventoryUtils {
+
+    /**
+     * @param inventory the target inventory
+     * @return the number of empty slots in {@code inventory}
+     */
+    public static int getNumberOfEmptySlotsInInventory(IItemHandler inventory) {
+        int emptySlots = 0;
+        for(int index = 0; index < inventory.getSlots(); index++) {
+            if(inventory.getStackInSlot(index).isEmpty()) {
+                emptySlots++;
+            }
+        }
+
+        return emptySlots;
+    }
+
+    /**
+     * Creates a deep copy of the target inventory, optionally keeping empty ItemStacks
+     *
+     * @param inventory the target inventory
+     * @param keepEmpty whether to keep empty ItemStacks (if {@code true}, stacks will be kept).
+     * @return a deep copy of the inventory.
+     */
+    public static List<ItemStack> deepCopy(IItemHandler inventory, boolean keepEmpty) {
+        int numSlots = inventory.getSlots();
+        List<ItemStack> inventoryStacks = new ArrayList<>(numSlots);
+        for(int slotIndex = 0; slotIndex < numSlots; slotIndex++) {
+            ItemStack stack = inventory.getStackInSlot(slotIndex);
+            if(keepEmpty || !stack.isEmpty())
+                inventoryStacks.add(stack.copy());
+        }
+        return inventoryStacks;
+    }
+
+    /**
+     * Determines whether all specified items will fit into a target inventory by
+     * simulating merging like items into existing stacks, then checking if there
+     * are enough empty stacks left to accommodate the remaining items.
+     * <br /><br />
+     * <b>Precondition:</b> the target inventory must not virtualize ItemStacks such that
+     *               they can exceed the maximum stackable size of the item as
+     *               defined by {@link net.minecraft.item.ItemStack#getMaxStackSize()}.
+     * <br /><br />
+     * <b>Precondition:</b> the target inventory must actually accept the types of items
+     *               you are trying to insert.
+     * <br /><br />
+     * @param items the items you want to insert
+     * @param inventory the target inventory receiving items
+     * @return {@code true} if inventory contains sufficient slots to merge and
+     *         insert all requested items, {@code false} otherwise.
+     */
+    public static boolean simulateItemStackMerge(List<ItemStack> items,
+                                                 IItemHandler inventory)
+    {
+        // If there's enough empty output slots then we don't need to compute merges.
+        final int emptySlots = getNumberOfEmptySlotsInInventory(inventory);
+        if(items.size() <= emptySlots)
+            return true;
+
+        // Deep copy the recipe output ItemStacks
+        final List<ItemStack> itemStacks = new ArrayList<>(items.size());
+        items.forEach(itemStack -> itemStacks.add(itemStack.copy()));
+
+        // Sort by the number of items in each stack so we merge smallest stacks first.
+        itemStacks.sort(Comparator.comparingInt(ItemStack::getCount));
+
+        // Deep copy the contents of the target inventory, skipping empty stacks
+        final List<ItemStack> inventoryStacks = deepCopy(inventory, false);
+
+        // Perform a merge of the ItemStacks
+        mergeItemStacks(itemStacks, inventoryStacks);
+
+        // Return whether there are now sufficient empty slots to fit the unmerged items.
+        return itemStacks.size() <= emptySlots;
+    }
+
+    /**
+     * Merges stacks of identical items from a source into a destination.<br />
+     * Successfully merged items will be removed from {@code source} and will appear in {@code destination}.<br />
+     * Empty stacks in {@code destination} are not considered for this process.
+     *
+     * @param source      the ItemStacks to merge into {@code destination}.
+     * @param destination a target inventory of existing ItemStacks.
+     */
+    private static void mergeItemStacks(Collection<ItemStack> source, Collection<ItemStack> destination) {
+        // Since we're mutating the collection during iteration, use an iterator.
+        final Iterator<ItemStack> sourceItemStacks = source.iterator();
+        while(sourceItemStacks.hasNext()) {
+            final ItemStack sourceItemStack = sourceItemStacks.next();
+
+            // Find a matching item in the output bus, if any
+            for(ItemStack destItemStack : destination)
+                if(ItemStack.areItemsEqual(destItemStack, sourceItemStack)) {
+                    // if it's possible to merge stacks
+                    final int availableSlots = destItemStack.getMaxStackSize() - destItemStack.getCount();
+                    if(availableSlots > 0) {
+                        final int itemCount = Math.min(availableSlots, sourceItemStack.getCount());
+                        sourceItemStack.shrink(itemCount);
+                        destItemStack.grow(itemCount);
+
+                        // if the output stack was merged completely, remove it and stop looking
+                        if(sourceItemStack.isEmpty()) {
+                            sourceItemStacks.remove();
+                            break;
+                        }
+                    }
+                }
+        }
+    }
+}
