479715fcf5dc5b24d8e5e3e45e8e9275f9877e97
ALongStringOfNumbers <sjensen793@gmail.com>
2020-12-15 21:06:51 -0700

    Fix the Primitive Blast Furnace Voiding outputs

--- src/main/java/gregtech/common/metatileentities/multi/MetaTileEntityPrimitiveBlastFurnace.java
+++ src/main/java/gregtech/common/metatileentities/multi/MetaTileEntityPrimitiveBlastFurnace.java
@@ -47,6 +47,8 @@ import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 
+import static gregtech.api.util.InventoryUtils.simulateItemStackMerge;
+
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
@@ -133,8 +135,11 @@ public class MetaTileEntityPrimitiveBlastFurnace extends MultiblockControllerBas
     }
 
     private boolean setupRecipe(ItemStack inputStack, int fuelAmount, PrimitiveBlastFurnaceRecipe recipe) {
+        List<ItemStack> outputs = new ArrayList<>();
+        outputs.add(recipe.getOutput());
+        outputs.add(getAshForRecipeFuelConsumption(recipe.getFuelAmount()));
         return inputStack.getCount() >= recipe.getInput().getCount() && fuelAmount >= recipe.getFuelAmount() &&
-            ItemHandlerHelper.insertItemStacked(exportItems, recipe.getOutput(), true).isEmpty();
+                simulateItemStackMerge(outputs, exportItems);
     }
 
     private boolean tryPickNewRecipe() {
