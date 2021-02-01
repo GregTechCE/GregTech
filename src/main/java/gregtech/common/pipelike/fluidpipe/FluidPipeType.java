5ac3f96b07fe1df3a99e29cc67b036bd5f056a95
DStrand1 <dane@strandboge.com>
2021-02-01 16:31:48 -0600

    Adjust pipe sizes for clarity

--- src/main/java/gregtech/common/pipelike/fluidpipe/FluidPipeType.java
+++ src/main/java/gregtech/common/pipelike/fluidpipe/FluidPipeType.java
@@ -6,9 +6,9 @@ import gregtech.api.unification.ore.OrePrefix;
 public enum FluidPipeType implements IMaterialPipeType<FluidPipeProperties> {
 
     TINY_OPAQUE("tiny", 0.20f, 1, OrePrefix.pipeTiny, true),
-    SMALL_OPAQUE("small", 0.30f, 2, OrePrefix.pipeSmall, true),
-    MEDIUM_OPAQUE("medium", 0.35f, 4, OrePrefix.pipeMedium, true),
-    LARGE_OPAQUE("large", 0.40f, 8, OrePrefix.pipeLarge, true);
+    SMALL_OPAQUE("small", 0.35f, 2, OrePrefix.pipeSmall, true),
+    MEDIUM_OPAQUE("medium", 0.50f, 4, OrePrefix.pipeMedium, true),
+    LARGE_OPAQUE("large", 0.65f, 8, OrePrefix.pipeLarge, true);
 
     public final String name;
     public final float thickness;
