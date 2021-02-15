e8dee99288d58d4c688ad1c8c77902348658adeb
Nikolay Korolev <CrafterKolyan@mail.ru>
2021-02-15 22:31:31 +0300

    Revert unused import not to have merge conflicts

--- src/main/java/gregtech/api/util/GTUtility.java
+++ src/main/java/gregtech/api/util/GTUtility.java
@@ -54,6 +54,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.List;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.function.Function;
