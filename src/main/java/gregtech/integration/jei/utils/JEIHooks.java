19b98f8963b82e1eedfc1cdc29a1149130d85f6a
DStrand1 <dane@strandboge.com>
2021-03-09 13:04:04 -0600

    Comment JEIHooks

--- src/main/java/gregtech/integration/jei/utils/JEIHooks.java
+++ src/main/java/gregtech/integration/jei/utils/JEIHooks.java
@@ -6,8 +6,16 @@ import net.minecraftforge.fluids.FluidStack;
 
 import java.util.List;
 
+/**
+ * Class used for code injection into JEI.
+ * Not intended for normal use within GTCE.
+ */
 public class JEIHooks {
 
+    /**
+     * This method is NOT intended to be called by GTCE.
+     * Do NOT use this method for any reason.
+     */
     public static void addFluidTooltip(List<String> tooltip, Object ingredient) {
         if (ingredient instanceof FluidStack) {
             String formula = GTUtility.getFluidFormula((FluidStack) ingredient);
