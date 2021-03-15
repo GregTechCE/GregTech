9c622beb21fd173d932893ad5b6a386574206e89
Salomão <salomaoo@gmail.com>
2021-03-15 20:31:42 -0300

    rework logic

--- src/main/java/gregtech/api/capability/impl/AbstractRecipeLogic.java
+++ src/main/java/gregtech/api/capability/impl/AbstractRecipeLogic.java
@@ -154,7 +154,6 @@ public abstract class AbstractRecipeLogic extends MTETrait implements IWorkable 
         IItemHandlerModifiable importInventory = getInputInventory();
         IMultipleTankHandler importFluids = getInputTank();
 
-        boolean inputsChanged = checkRecipeInputsDirty(importInventory, importFluids);
         // if the output is full check if the output changed so we can process recipes results again.
         if (this.isOutputsFull) {
             IItemHandlerModifiable exportInventory = getOutputInventory();
@@ -164,11 +163,14 @@ public abstract class AbstractRecipeLogic extends MTETrait implements IWorkable 
             }
         }
 
-        if (inputsChanged || (!invalidInputsForRecipes && !this.isOutputsFull) || this.forceRecipeRecheck) {
+        if (!invalidInputsForRecipes && !this.isOutputsFull) {
             if (this.previousRecipe != null && this.previousRecipe.matches(false, importInventory, importFluids)) {
                 //if previous recipe still matches inputs, try to use it
                 currentRecipe = this.previousRecipe;
-            } else {
+            }
+        }
+        if (currentRecipe == null) {
+            if (checkRecipeInputsDirty(importInventory, importFluids) || this.forceRecipeRecheck) {
                 this.forceRecipeRecheck = false;
                 //else, try searching new recipe for given inputs
                 currentRecipe = findRecipe(maxVoltage, importInventory, importFluids);
@@ -181,9 +183,9 @@ public abstract class AbstractRecipeLogic extends MTETrait implements IWorkable 
                     this.invalidInputsForRecipes = true;
                 }
             }
-            if (currentRecipe != null && setupAndConsumeRecipeInputs(currentRecipe)) {
-                setupRecipe(currentRecipe);
-            }
+        }
+        if (currentRecipe != null && setupAndConsumeRecipeInputs(currentRecipe)) {
+            setupRecipe(currentRecipe);
         }
     }
 
@@ -312,6 +314,7 @@ public abstract class AbstractRecipeLogic extends MTETrait implements IWorkable 
             this.isOutputsFull = true;
             return false;
         }
+        this.isOutputsFull = false;
         return recipe.matches(true, importInventory, importFluids);
     }
 
--- src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityMultiFurnace.java
+++ src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityMultiFurnace.java
@@ -106,7 +106,6 @@ public class MetaTileEntityMultiFurnace extends RecipeMapMultiblockController {
             IItemHandlerModifiable importInventory = getInputInventory();
             IMultipleTankHandler importFluids = getInputTank();
 
-            boolean inputsChanged = checkRecipeInputsDirty(importInventory, importFluids);
             if (this.isOutputsFull) {
                 IItemHandlerModifiable exportInventory = getOutputInventory();
                 IMultipleTankHandler exportFluids = getOutputTank();
@@ -119,18 +118,24 @@ public class MetaTileEntityMultiFurnace extends RecipeMapMultiblockController {
             //for MultiSmelter, we can reuse previous recipe if inputs didn't change
             //otherwise, we need to recompute it for new ingredients
             //but technically, it means we can cache multi smelter recipe, but changing inputs have more priority
-            if (inputsChanged || (!invalidInputsForRecipes && !this.isOutputsFull) || this.forceRecipeRecheck) {
+            if (checkRecipeInputsDirty(importInventory, importFluids) || this.forceRecipeRecheck) {
                 this.forceRecipeRecheck = false;
                 //else, try searching new recipe for given inputs
                 currentRecipe = findRecipe(maxVoltage, importInventory, importFluids);
                 if (currentRecipe != null) {
                     this.previousRecipe = currentRecipe;
                 }
-                if (currentRecipe != null && setupAndConsumeRecipeInputs(currentRecipe)) {
-                    setupRecipe(currentRecipe);
+            } else if (!invalidInputsForRecipes && !this.isOutputsFull) {
+                if (previousRecipe != null && previousRecipe.matches(false, importInventory, importFluids)) {
+                    //if previous recipe still matches inputs, try to use it
+                    currentRecipe = previousRecipe;
                 }
             }
+            if (currentRecipe != null && setupAndConsumeRecipeInputs(currentRecipe)) {
+                setupRecipe(currentRecipe);
+            }
         }
+
 
         @Override
         protected Recipe findRecipe(long maxVoltage,
