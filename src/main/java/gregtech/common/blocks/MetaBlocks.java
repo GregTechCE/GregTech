2296527c70eca3bafa1eae9574a08f9983dcae12
phantamanta44 <evandalong@gmail.com>
2021-03-21 15:10:17 -0500

    Map meta block IDs from material ID 0, compute lookup tables for old meta block mapping

--- src/main/java/gregtech/common/blocks/MetaBlocks.java
+++ src/main/java/gregtech/common/blocks/MetaBlocks.java
@@ -1,6 +1,8 @@
 package gregtech.common.blocks;
 
 import com.google.common.collect.ImmutableMap;
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
 import gregtech.api.GTValues;
 import gregtech.api.GregTechAPI;
 import gregtech.api.block.machines.BlockMachine;
@@ -107,7 +109,9 @@ public class MetaBlocks {
     public static BlockCrusherBlade CRUSHER_BLADE;
 
     public static Map<DustMaterial, BlockCompressed> COMPRESSED = new HashMap<>();
+    public static TIntObjectMap<int[]> COMPRESSED_OLD = new TIntObjectHashMap<>();
     public static Map<IngotMaterial, BlockSurfaceRockDeprecated> SURFACE_ROCKS = new HashMap<>();
+    public static TIntObjectMap<int[]> SURFACE_ROCKS_OLD = new TIntObjectHashMap<>();
     public static BlockSurfaceRockNew SURFACE_ROCK_NEW;
     public static Map<SolidMaterial, BlockFrame> FRAMES = new HashMap<>();
     public static Collection<BlockOre> ORES = new HashSet<>();
@@ -171,11 +175,16 @@ public class MetaBlocks {
         createGeneratedBlock(
             material -> material instanceof DustMaterial && !OrePrefix.block.isIgnored(material),
             MetaBlocks::createCompressedBlock);
-
-        // TODO: We may want to switch this to use the new mapping scheme
         createGeneratedBlockDeprecated(
+            material -> material instanceof DustMaterial && !OrePrefix.block.isIgnored(material),
+            generateInto(COMPRESSED_OLD));
+
+        createGeneratedBlock(
             material -> material instanceof IngotMaterial && material.hasFlag(MatFlags.GENERATE_ORE),
             MetaBlocks::createSurfaceRockBlock);
+        createGeneratedBlockDeprecated(
+            material -> material instanceof IngotMaterial && material.hasFlag(MatFlags.GENERATE_ORE),
+            generateInto(SURFACE_ROCKS_OLD));
 
         for (Material material : Material.MATERIAL_REGISTRY) {
             if (material instanceof DustMaterial &&
@@ -221,8 +230,7 @@ public class MetaBlocks {
 
         for(Material material : Material.MATERIAL_REGISTRY)
             if(materialPredicate.test(material)) {
-                // IDs start at 1, we start at 0.
-                int id = Material.MATERIAL_REGISTRY.getIDForObject(material) - 1;
+                int id = Material.MATERIAL_REGISTRY.getIDForObject(material);
                 int metaBlockID = id / 16;
                 int subBlockID = id % 16;
 
@@ -271,9 +279,14 @@ public class MetaBlocks {
         return (currentGenerationIndex / 16) + 1;
     }
 
+    private static BiConsumer<Material[], Integer> generateInto(TIntObjectMap<int[]> dest) {
+        return (materials, index) -> dest.put(
+                index, Arrays.stream(materials).mapToInt(Material.MATERIAL_REGISTRY::getIDForObject).toArray());
+    }
+
     private static void createSurfaceRockBlock(Material[] materials, int index) {
         BlockSurfaceRockDeprecated block = new BlockSurfaceRockDeprecated(materials);
-        block.setRegistryName("surface_rock_" + index);
+        block.setRegistryName("meta_block_surface_rock_" + index);
         for (Material material : materials) {
             if (material instanceof IngotMaterial) {
                 SURFACE_ROCKS.put((IngotMaterial) material, block);
@@ -283,7 +296,7 @@ public class MetaBlocks {
 
     private static void createCompressedBlock(Material[] materials, int index) {
         BlockCompressed block = new BlockCompressed(materials);
-        block.setRegistryName("compressed_" + index);
+        block.setRegistryName("meta_block_compressed_" + index);
         for (Material material : materials) {
             if (material instanceof DustMaterial) {
                 COMPRESSED.put((DustMaterial) material, block);
