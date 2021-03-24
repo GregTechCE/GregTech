1468c30defe418d285d7aed49a6966c0a5cafcaa
DStrand1 <dane@strandboge.com>
2021-03-24 13:22:06 -0500

    Add javadoc for getZoom()

--- src/main/java/gregtech/integration/jei/multiblock/IMultiblockInfoPage.java
+++ src/main/java/gregtech/integration/jei/multiblock/IMultiblockInfoPage.java
@@ -12,6 +12,10 @@ public interface IMultiblockInfoPage {
 
     String[] getDescription();
 
+    /**
+     * Used to set the Zoom of the JEI Multiblock Preview.
+     * @return The zoom as a float, in the range (0.0f, 1.0f]
+     */
     default float getZoom() {
         return 1.0f;
     }
