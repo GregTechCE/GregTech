aaafd33f1657d4b529be508b06d6e31d31c847ba
DStrand1 <dane@strandboge.com>
2021-04-10 22:03:03 -0500

    Fix javadoc typo (C -> K)

--- src/main/java/gregtech/api/unification/material/type/IngotMaterial.java
+++ src/main/java/gregtech/api/unification/material/type/IngotMaterial.java
@@ -64,8 +64,8 @@ public class IngotMaterial extends SolidMaterial {
     /**
      * Blast furnace temperature of this material
      * Equal to zero if material doesn't use blast furnace
-     * If below 1000C, primitive blast furnace recipes will be also added.
-     * If above 1750C, a Hot Ingot and its Vacuum Freezer recipe will be also added.
+     * If below 1000K, primitive blast furnace recipes will be also added.
+     * If above 1750K, a Hot Ingot and its Vacuum Freezer recipe will be also added.
      */
     @ZenProperty("blastFurnaceTemp")
     public int blastFurnaceTemperature;
