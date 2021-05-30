754e1b4f120d414f28eb5fa48c67374adbada32b
Salomão <salomaoo@gmail.com>
2021-05-30 14:41:56 -0300

    update javadoc

--- src/main/java/gregtech/api/capability/INotifiableHandler.java
+++ src/main/java/gregtech/api/capability/INotifiableHandler.java
@@ -9,8 +9,7 @@ import gregtech.api.metatileentity.MetaTileEntity;
 public interface INotifiableHandler {
 
     /**
-     * Notifies the entities of the changes in their inventories
-     * An iterator is used to remove invalid TileEntities
+     * Adds the notified handler to the notified list
      *
      * @param isExport boolean specifying if a handler is an output handler
      */
