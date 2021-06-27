16fe063a2fa23b861f518f722f77672baed8367c
DStrand1 <dane@strandboge.com>
2021-06-27 00:32:16 -0500

    rework pipe collision code

--- src/main/java/gregtech/common/EventHandlers.java
+++ src/main/java/gregtech/common/EventHandlers.java
@@ -1,25 +1,17 @@
 package gregtech.common;
 
 import gregtech.api.GTValues;
-import gregtech.api.capability.GregtechCapabilities;
-import gregtech.api.pipenet.block.BlockPipe;
-import gregtech.api.util.GTUtility;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityEnderman;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class EventHandlers {
@@ -53,20 +45,5 @@ public class EventHandlers {
         if (event.getModID().equals(GTValues.MODID)) {
             ConfigManager.sync(GTValues.MODID, Config.Type.INSTANCE);
         }
-    }
-
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public static void onPlayerInventoryEvent(LivingEvent.LivingUpdateEvent event) {
-        EntityLivingBase entity = event.getEntityLiving();
-        if (entity instanceof EntityPlayer)
-            BlockPipe.isToolHeld.set(isPipeCollisionChangingItem(entity.getHeldItemMainhand()));
-    }
-
-    private static boolean isPipeCollisionChangingItem(ItemStack itemStack) {
-        return itemStack.hasCapability(GregtechCapabilities.CAPABILITY_CUTTER, null) |
-                itemStack.hasCapability(GregtechCapabilities.CAPABILITY_WRENCH, null) ||
-                itemStack.hasCapability(GregtechCapabilities.CAPABILITY_SCREWDRIVER, null) ||
-                GTUtility.isCoverBehaviorItem(itemStack);
     }
 }
