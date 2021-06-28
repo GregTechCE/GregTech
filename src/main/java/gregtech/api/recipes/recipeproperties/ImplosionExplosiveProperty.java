4c697d810f26876a892a5f5128de8eff5f23d7f4
ALongStringOfNumbers <31759736+ALongStringOfNumbers@users.noreply.github.com>
2021-06-28 09:57:29 -0700

    Update systems to new Recipe Properties (#1672)

---
+++ src/main/java/gregtech/api/recipes/recipeproperties/ImplosionExplosiveProperty.java
@@ -0,0 +1,36 @@
+package gregtech.api.recipes.recipeproperties;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+
+public class ImplosionExplosiveProperty extends RecipeProperty<ItemStack>{
+
+    private static final String KEY = "explosives";
+
+    private static ImplosionExplosiveProperty INSTANCE;
+
+
+    private ImplosionExplosiveProperty() {
+        super(KEY, ItemStack.class);
+    }
+
+    public static ImplosionExplosiveProperty getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ImplosionExplosiveProperty();
+        }
+
+        return INSTANCE;
+    }
+
+    @Override
+    public void drawInfo(Minecraft minecraft, int x, int y, int color, Object value) {
+        minecraft.fontRenderer.drawString(I18n.format("gregtech.recipe.explosive",
+                ((ItemStack) value).getDisplayName()), x, y, color);
+    }
+
+    @Override
+    public boolean isHidden() {
+        return true;
+    }
+}
