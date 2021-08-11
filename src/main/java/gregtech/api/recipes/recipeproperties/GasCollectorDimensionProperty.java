292ff214ef05a3fa5b6f71ce3feb1b2d82af75ea
Tech22 <swarren2022@gmail.com>
2021-08-11 18:47:10 -0400

    use dimension names for gas collector property

--- src/main/java/gregtech/api/recipes/recipeproperties/GasCollectorDimensionProperty.java
+++ src/main/java/gregtech/api/recipes/recipeproperties/GasCollectorDimensionProperty.java
@@ -1,9 +1,11 @@
 package gregtech.api.recipes.recipeproperties;
 
+import gregtech.api.worldgen.config.WorldGenRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 
 import java.util.List;
+import java.util.Map;
 
 public class GasCollectorDimensionProperty extends RecipeProperty<List> {
     public static final String KEY = "dimension";
@@ -23,13 +25,14 @@ public class GasCollectorDimensionProperty extends RecipeProperty<List> {
     @Override
     public void drawInfo(Minecraft minecraft, int x, int y, int color, Object value) {
         minecraft.fontRenderer.drawString(I18n.format("gregtech.recipe.dimensions",
-                value, getDimensionsForRecipe(castValue(value))), x, y, color);
+                getDimensionsForRecipe(castValue(value))), x, y, color);
     }
 
     private String getDimensionsForRecipe(List<Integer> value) {
+        Map<Integer, String> dimNames = WorldGenRegistry.getNamedDimensions();
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < value.size(); i++) {
-            builder.append(value.get(i));
+            builder.append(dimNames.getOrDefault(value.get(i), "" + value.get(i)));
             if (i != value.size() - 1)
                 builder.append(", ");
         }
