605ed5e240e8cbb4425eb850135bb0aa7b8ed3de
DStrand1 <dane@strandboge.com>
2021-09-04 17:59:14 -0500

    rewrite LighterBehavior, maybe fix lag?

--- src/main/java/gregtech/common/items/behaviors/LighterBehaviour.java
+++ src/main/java/gregtech/common/items/behaviors/LighterBehaviour.java
@@ -1,14 +1,17 @@
 package gregtech.common.items.behaviors;
 
+import gregtech.api.GTValues;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -32,23 +35,18 @@ public class LighterBehaviour extends AbstractUsableBehaviour {
 
     @Override
     public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
+        pos = pos.offset(side);
         ItemStack stack = player.getHeldItem(hand);
         if (!player.canPlayerEdit(pos, side, stack)) {
             return EnumActionResult.FAIL;
+        } else {
+            if (world.isAirBlock(pos)) {
+                world.playSound(player, pos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0f, GTValues.RNG.nextFloat() * 0.4f + 0.8f);
+                world.setBlockState(pos, Blocks.FIRE.getDefaultState(), 11);
+            }
+            useItemDurability(player, hand, stack, ItemStack.EMPTY);
+            return EnumActionResult.SUCCESS;
         }
-        if (!tryIgniteBlock(world, pos.offset(side))) {
-            return EnumActionResult.PASS;
-        }
-        useItemDurability(player, hand, stack, ItemStack.EMPTY);
-        return EnumActionResult.SUCCESS;
-    }
-
-    public boolean tryIgniteBlock(World world, BlockPos pos) {
-        if (world.isAirBlock(pos)) {
-            world.setBlockState(pos, Blocks.FIRE.getDefaultState());
-            return true;
-        }
-        return false;
     }
 
     @Override
