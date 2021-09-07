c8fa00489583a2172938988e00da45083c17ba6d
TechLord22 <swarren2022@gmail.com>
2021-09-07 09:45:41 -0400

    better fisher tooltip

--- src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityFisher.java
+++ src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityFisher.java
@@ -140,7 +140,7 @@ public class MetaTileEntityFisher extends TieredMetaTileEntity {
     public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
         tooltip.add(I18n.format("gregtech.machine.fisher.tooltip"));
         tooltip.add(I18n.format("gregtech.machine.fisher.speed", fishingTicks));
-        tooltip.add(I18n.format("gregtech.machine.fisher.requirement", (int) Math.sqrt(WATER_CHECK_SIZE)));
+        tooltip.add(I18n.format("gregtech.machine.fisher.requirement", (int) Math.sqrt(WATER_CHECK_SIZE), (int) Math.sqrt(WATER_CHECK_SIZE)));
         tooltip.add(I18n.format("gregtech.universal.tooltip.voltage_in", energyContainer.getInputVoltage(), GTValues.VN[getTier()]));
         tooltip.add(I18n.format("gregtech.universal.tooltip.energy_storage_capacity", energyContainer.getEnergyCapacity()));
     }
