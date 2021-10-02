7230afe080702521f35f550abdcabc85ab846817
Exaxxion <Exaxxion@users.noreply.github.com>
2021-10-02 01:26:38 -0700

    Updated Changelog and version bump to 1.17.1 (#1720)
    Co-authored-by: LAGIdiot <bordel.muj@gmail.com>

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -8,7 +8,7 @@ public final class GregTechVersion {
     //This number is incremented every major feature update
     public static final int MINOR = 17;
     //This number is incremented every time the feature is added, or bug is fixed. resets every major version change
-    public static final int REVISION = 0;
+    public static final int REVISION = 1;
     //This number is incremented every build, and never reset. Should always be 0 in the repo code.
     public static final int BUILD = 0;
 
