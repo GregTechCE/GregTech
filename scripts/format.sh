6e20995f116b03f01e3958f807659e7503d96424
DStrand1 <dane@strandboge.com>
2021-11-19 23:17:04 -0600

    add formatting script

---
+++ scripts/format.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+npm install -g @htmlacademy/editorconfig-cli
+IFS=$'\n' read -rd '' -a FILES <<< "$(git --no-pager diff --name-only FETCH_HEAD "$(git merge-base FETCH_HEAD master)")"
+ERROR=""
+for i in "${FILES[@]}"
+do
+    if [[ "$i" == *".java"* || "$i" == *".json"* ]]
+    then
+        if ! editorconfig-cli -e ../.editorconfig "../$i" > /dev/null
+        then
+            ERROR="error"
+        fi
+    fi
+done
+
+if [[ "$ERROR" != "" ]]
+then
+    echo "Found files with improper formatting"
+    exit 1
+else
+    echo "No formatting issues"
+fi
