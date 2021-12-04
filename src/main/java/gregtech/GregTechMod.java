7505071059830f9a848b4578690dbe0f5414ab43
froot <66188216+kumquat-ir@users.noreply.github.com>
2021-12-03 19:31:00 -0800

    do not load before ctm (#287)

--- src/main/java/gregtech/GregTechMod.java
+++ src/main/java/gregtech/GregTechMod.java
@@ -49,7 +49,7 @@ import static gregtech.api.GregTechAPI.*;
 @Mod(modid = GTValues.MODID,
         name = "GregTech",
         acceptedMinecraftVersions = "[1.12,1.13)",
-        dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + "after:forestry;after:jei@[4.15.0,);after:crafttweaker;before:ctm")
+        dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + "after:forestry;after:jei@[4.15.0,);after:crafttweaker")
 public class GregTechMod {
 
     static {
