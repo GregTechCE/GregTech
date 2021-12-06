1629e49558c5961da8d4610a6f5f1c898542ea47
DStrand1 <dane@strandboge.com>
2021-12-05 18:13:47 -0600

    fix steam grinder granting byproducts

--- src/main/java/gregtech/common/metatileentities/multi/steam/MetaTileEntitySteamGrinder.java
+++ src/main/java/gregtech/common/metatileentities/multi/steam/MetaTileEntitySteamGrinder.java
@@ -8,6 +8,8 @@ import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.RecipeMapSteamMultiblockController;
 import gregtech.api.multiblock.BlockPattern;
 import gregtech.api.multiblock.FactoryBlockPattern;
+import gregtech.api.recipes.Recipe;
+import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.render.ICubeRenderer;
 import gregtech.api.render.OrientedOverlayRenderer;
@@ -20,6 +22,8 @@ import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
 
+import java.util.Collections;
+
 import static gregtech.api.render.Textures.BRONZE_PLATED_BRICKS;
 import static gregtech.api.render.Textures.SOLID_STEEL_CASING;
 
@@ -31,7 +35,17 @@ public class MetaTileEntitySteamGrinder extends RecipeMapSteamMultiblockControll
 
     public MetaTileEntitySteamGrinder(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId, RecipeMaps.MACERATOR_RECIPES, CONVERSION_RATE);
-        this.recipeMapWorkable = new SteamMultiWorkable(this, CONVERSION_RATE);
+        this.recipeMapWorkable = new SteamMultiWorkable(this, CONVERSION_RATE) {
+            @Override
+            public void applyParallelBonus(RecipeBuilder<?> builder) {
+                super.applyParallelBonus(builder);
+                if (builder.getOutputs().size() == 0) {
+                    Recipe.ChanceEntry output = builder.getChancedOutputs().get(0);
+                    builder.clearChancedOutput();
+                    builder.chancedOutputs(Collections.singletonList(output));
+                } else builder.clearChancedOutput();
+            }
+        };
         this.recipeMapWorkable.setParallelLimit(8);
     }
 
