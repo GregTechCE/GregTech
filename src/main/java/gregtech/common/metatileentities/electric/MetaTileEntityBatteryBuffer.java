fbc2468602d0d67cb0c854a57d7c36f85b3c230c
DStrand1 <dane@strandboge.com>
2021-12-25 21:44:46 -0600

    Fix battery buffer UI

--- src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityBatteryBuffer.java
+++ src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityBatteryBuffer.java
@@ -140,10 +140,10 @@ public class MetaTileEntityBatteryBuffer extends TieredMetaTileEntity implements
                         18 + 18 * colSize + 94)
                 .label(6, 6, getMetaFullName());
 
-        for (int x = 0; x < rowSize; x++) {
-            for (int y = 0; y < colSize; y++) {
-                int index = y * colSize + x;
-                builder.widget(new SlotWidget(importItems, index, 88 - rowSize * 9 + x * 18, 18 + y * 18, true, true)
+        int index = 0;
+        for (int y = 0; y < colSize; y++) {
+            for (int x = 0; x < rowSize; x++) {
+                builder.widget(new SlotWidget(importItems, index++, 88 - rowSize * 9 + x * 18, 18 + y * 18, true, true)
                         .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.BATTERY_OVERLAY));
             }
         }
