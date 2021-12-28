5cc3e75470548b8ac8754d80c2905225f10080d0
DStrand1 <dane@strandboge.com>
2021-12-28 01:16:12 -0600

    Bump from alpha to beta

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -8,7 +8,7 @@ public final class GregTechVersion {
     //This number is incremented every time the feature is added, or bug is fixed. resets every major version change
     public static final int REVISION = 0;
     //This String is for additional info on the release version if needed (like alpha, beta, rc). Comment out when unused
-    public static final String EXTRA = "alpha";
+    public static final String EXTRA = "beta";
     //This is the String formatted version, used in builds
     public static final String VERSION = "@VERSION@";
 
